62ee9b757aebd33f62ca3f046c26c79ee996dc1c
pxbloruben <rubenpablo33@gmail.com>
2025-02-10 21:47:59 -0600

    agrego mi primer ejercicio del modulo 1

---
+++ tipos-de-datos.js
@@ -0,0 +1,20 @@
+//Este es un programa que muestra los tipos de datos en JavaScript
+console.log("Tipos de datos");
+console.log(typeof 42);
+ // Número
+console.log(typeof 'Twenty20');
+ // string
+console.log(typeof '123'); 
+// string
+console.log(typeof -68); 
+// Número
+console.log(typeof true); 
+// boolean
+console.log(typeof FALSE); 
+// INDEFINIDO
+console.log(typeof null); 
+// objecto
+console.log(typeof undefined);
+ // undefined
+console.log(typeof 0); 
+// numero 
